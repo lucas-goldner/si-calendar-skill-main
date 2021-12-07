139fded20686a3bfeaed473909734dcc64c5b122
Lea Gutierrez Lange <lg067@hdm-stuttgart.de>
2021-12-07 09:48:25 +0100

    pdf update

--- __init__.py
+++ __init__.py
@@ -6,9 +6,16 @@ class SiCalendar(MycroftSkill):
         MycroftSkill.__init__(self)
 
     @intent_file_handler('calendar.si.intent')
-    def handle_calendar_si(self, message):
+    def return_appointment_for_Date(self, message):
+        #date = message.data('date')
+        #events_fetched = my_new_calendar.date_search(
+        #start=datetime(2021, 1, 1), end=datetime(2024, 1, 1), expand=True)
         self.speak_dialog('calendar.si')
-
+    
+    
+    @intent_file_handler('calendar.nextAppointment.intent')
+    def return_next_appointment(self, message):
+        self.speak_dialog('calendar.si')
 
 def create_skill():
     return SiCalendar()
