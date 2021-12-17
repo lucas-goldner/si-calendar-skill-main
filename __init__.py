590df66b9413f55eee3458391499941a124335f3
lucas-goldner <lucas.goldner@googlemail.com>
2021-12-17 16:07:18 +0100

    feat: Add config fetching

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from mycroft import MycroftSkill, intent_file_handler
 from mycroft.util.format import nice_time, nice_date
 from datetime import datetime, date
+import caldav
 
 
 class SiCalendar(MycroftSkill):
@@ -9,7 +10,15 @@ class SiCalendar(MycroftSkill):
 
     @intent_file_handler('calendar.si.intent')
     def handle_calendar_si(self, message):
-        self.speak_dialog('calendar.si', data = {"date": nice_date(datetime.now())})
+        filename = "/etc/si-calendar/cal_config.txt"
+        listOfLines=[]
+        with open(filename,"r",encoding="uft-8") as fin:
+            for line in fin:
+                line = line.strip()
+                listOfLines.append(line)
+        self.client = caldav.DAVClient("https://nextcloud.humanoidlab.hdm-stuttgart.de/remote.php/dav", username=listOfLines[0], password=listOfLines[1])
+        self.speak_dialog('calendar.si', data = {"mail": listOfLines[0], "password": listOfLines[1]})
+        #self.speak_dialog('calendar.si', data = {"date": nice_date(datetime.now())})
 
 
 def create_skill():
