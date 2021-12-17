246e1affdb759958aafc721df99362f804afce02
lucas-goldner <lucas.goldner@googlemail.com>
2021-12-17 15:40:25 +0100

    feat: Add nice_date

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
 from mycroft import MycroftSkill, intent_file_handler
+from mycroft.util.format import nice_time, nice_date
+from datetime import datetime, date
 
 
 class SiCalendar(MycroftSkill):
@@ -7,7 +9,7 @@ class SiCalendar(MycroftSkill):
 
     @intent_file_handler('calendar.si.intent')
     def handle_calendar_si(self, message):
-        self.speak_dialog('calendar.si', data = {"name": "Lucas"})
+        self.speak_dialog('calendar.si', data = {"date": date(2002, 12, 4)})
 
 
 def create_skill():
