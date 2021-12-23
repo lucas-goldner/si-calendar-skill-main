8bde75edced72698b8006d4463ef5d926a73d272
lucas-goldner <lucas.goldner@googlemail.com>
2021-12-23 18:54:03 +0100

    fix: Format of date for is_full_day

--- __init__.py
+++ __init__.py
@@ -7,18 +7,18 @@ import caldav
 class SiCalendar(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
-
-    @intent_file_handler('calendar.si.intent')
-    def handle_calendar_si(self, message):
-        appointments = []
         filename = "/etc/si-calendar/cal-config.txt"
         listOfLines=[]
         with open(filename,"r",encoding="utf-8") as fin:
             for line in fin:
                 line = line.strip()
                 listOfLines.append(line)
-        client = caldav.DAVClient("https://nextcloud.humanoidlab.hdm-stuttgart.de/remote.php/dav", username=listOfLines[0], password=listOfLines[1])
-        principal = client.principal()
+        self.client = caldav.DAVClient("https://nextcloud.humanoidlab.hdm-stuttgart.de/remote.php/dav", username=listOfLines[0], password=listOfLines[1])
+
+    @intent_file_handler('calendar.si.intent')
+    def handle_calendar_si(self, message):
+        appointments = []
+        principal = self.client.principal()
         for calendar in principal.calendars():
             for event in calendar.events():
                 ical_text = event.data
@@ -57,7 +57,7 @@ class SiCalendar(MycroftSkill):
                     day = string_cut[doublepoint_index + 1:dtendindex][6:8]
                     hour = string_cut[doublepoint_index + 1:dtendindex][9:11]
                     minute = string_cut[doublepoint_index + 1:dtendindex][11:13]
-                    date_time_str = day + "." + month + "." + year + " " + hour + ":" + minute
+                    date_time_str = day + "/" + month + "/" + year + " " + hour + ":" + minute
                     date_time_obj = datetime.strptime(date_time_str, '%d/%m/%y %H:%M')
                     appointments.append({"name": summary, "date": date_time_obj})
 
