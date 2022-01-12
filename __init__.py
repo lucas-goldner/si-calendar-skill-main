5698fed1c4f7997eea981a6f5eee3e0454031e62
lucas-goldner <lucas.goldner@googlemail.com>
2022-01-12 19:00:45 +0100

    fix: Go back

--- __init__.py
+++ __init__.py
@@ -15,7 +15,7 @@ class SiCalendar(MycroftSkill):
                 listOfLines.append(line)
         self.client = caldav.DAVClient("https://nextcloud.humanoidlab.hdm-stuttgart.de/remote.php/dav", username=listOfLines[0], password=listOfLines[1])
 
-    @intent_file_handler('multiple_ap.si.intent')
+    @intent_file_handler('calendar.si.intent')
     def handle_calendar_si(self, message):
         appointments = self.fetch_events()
         #Filters for appointments that are sooner than the present date and orders them by occurence
